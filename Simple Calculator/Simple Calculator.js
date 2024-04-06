c840372c52d5f739976e6c672495cc5c5aef3762
ahmedworlds <accountsworlds@gmail.com>
2024-04-06 20:48:42 +0200

    Update Simple Calculator.js

--- Simple Calculator/Simple Calculator.js
+++ Simple Calculator/Simple Calculator.js
@@ -1,4 +1,5 @@
 // program for a simple calculator
+let result;
 
 // take the operator input
 const operator = prompt('Enter operator ( either +, -, * or / ): ');
@@ -7,21 +8,28 @@ const operator = prompt('Enter operator ( either +, -, * or / ): ');
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
 
-let result;
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
 
-// using if...else if... else
-if (operator == '+') {
-    result = number1 + number2;
-}
-else if (operator == '-') {
-    result = number1 - number2;
-}
-else if (operator == '*') {
-    result = number1 * number2;
-}
-else {
-    result = number1 / number2;
-}
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
 
-// display the result
-console.log(`${number1} ${operator} ${number2} = ${result}`);
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
